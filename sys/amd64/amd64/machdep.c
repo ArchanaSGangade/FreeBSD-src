a344232c5d4a5f0c0b5bd802a8936bf4d599cb5f
AkshataMullya <32295740+AkshataMullya@users.noreply.github.com>
2018-05-16 13:16:53 +0530

    Update machdep.c

--- sys/amd64/amd64/machdep.c
+++ sys/amd64/amd64/machdep.c
@@ -1651,7 +1651,7 @@ hammer_time(u_int64_t modulep, u_int64_t physfree)
 	    != NULL)
 		vty_set_preferred(VTY_VT);
 
-	identify_cpu();		/* Final stage of CPU initialization */
+	finishidentcpu();		/* Final stage of CPU initialization */
 	initializecpu();	/* Initialize CPU registers */
 	initializecpucache();
 
